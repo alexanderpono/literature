6591edfb039a933d7dd74b9fc8dd9389d5ea89bb
Alexander Ponomarenko <brain16383@yandex.ru>
2024-07-24 22:53:05 +0300

    Add some stories #2

--- src/stories.ts
+++ src/stories.ts
@@ -168,5 +168,157 @@ export const stories: Story[] = [
         type: StoryType.STORY,
         caption: 'А.П. Чехов. Рассказы. «Хирургия»',
         url: 'https://audioskazki-online.ru/rasskazy/chekhov-anton/khirurgiya?ysclid=lxkwwfdoyf39711030'
+    },
+    {
+        type: StoryType.HEADER1,
+        caption: 'Русская литература 20 века'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'И.А. Бунин «В деревне»',
+        url: 'https://audioskazki-online.ru/rasskazy/ivan-bunin/derevnya?ysclid=lz07o5nbnc374365008'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'И.А. Бунин «Лапти»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/lapti?ysclid=lz07pyp1ek665617991'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'В.Г. Короленко. Повесть «В дурном обществе»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/v-durnom-obshchestve?ysclid=lz07qi3ilx464399128'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'С. Есенин. Стихотворения',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/stikhi-esenina?ysclid=lz07rnad4860553677'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'П.П. Бажов. Сказы. «Медной горы хозяйка»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/mednoj-gory-khozyajka?ysclid=lz07ugr19l365545310'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'П.П. Бажов. Сказы. «Малахитовая шкатулка»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/malakhitovaya-shkatulka?ysclid=lz07uxy5nu458689862'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'П.П. Бажов. Сказы. «Каменный цветок»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/kamennyj-tsvetok?ysclid=lz07voql5o501053837'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'К.Г. Паустовский. «Теплый хлеб»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/teplyj-khleb?ysclid=lz07x01o1g988640007'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'К.Г. Паустовский. «Заячьи лапы»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/zayachi-lapy?ysclid=lz07xlhgih694868574'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'М. Пришвин. «Кладовая солнца»',
+        url: 'https://audioskazki-online.ru/rasskazy/prishvin-mihail/kladovaya-solntsa?ysclid=lz07xyf861184286678'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'С.Я. Маршак. Пьеса «Двенадцать месяцев»',
+        url: 'https://audioskazki-online.ru/russkie-audioskazki/samuil-marshak/12-mesyatsev?ysclid=lz07z7ij4c808010011'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'А.П. Платонов. Рассказ «Никита»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/nikita?ysclid=lz080818z2584804648'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'В.П. Астафьев. Рассказ «Васюткино озеро»',
+        url: 'https://audioskazki-online.ru/rasskazy/astafev-viktor/vasyutkino-ozero?ysclid=lz080pt92e129245509'
+    },
+
+    {
+        type: StoryType.HEADER1,
+        caption: 'Поэты о Великой отечественной войне (1941-1945гг.)'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'А.Т. Твардовский. «Василий Тёркин»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/vasilij-tjorkin?ysclid=lz082w0r6x653616655'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'К.М. Симонов. «Сын артиллериста» (читает автор)',
+        url: 'https://my.mail.ru/music/songs/%D0%BA%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD-%D1%81%D0%B8%D0%BC%D0%BE%D0%BD%D0%BE%D0%B2-%D1%81%D1%8B%D0%BD-%D0%B0%D1%80%D1%82%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B8%D1%81%D1%82%D0%B0-%D1%87%D0%B8%D1%82%D0%B0%D0%B5%D1%82-%D0%B0%D0%B2%D1%82%D0%BE%D1%80-db744f85bc3b53d92c1bc0b6f4e440d5?ysclid=lz084ii2qa802889488'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'К.М. Симонов. «Майор привёз мальчишку на лафете»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/major-privjoz-malchishku-na-lafete?ysclid=lz0861znkc644571210'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'И.А. Бунин. Рассказы',
+        url: 'https://audioskazki-online.ru/rasskazy/ivan-bunin?ysclid=lz08qdx4tx207034543'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Д.Б. Кедрин «Алёнушка»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/aljonushka-kedrin?ysclid=lz08renb3u785138355'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'А.А. Прокофьев «Алёнушка»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/aljonushka?ysclid=lz08t882ls360164125'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Н.М. Рубцов. «Сентябрь»',
+        url: 'https://audioskazki-online.ru/podborki/raznoe/sentyabr?ysclid=lz08wiu6tb515058356'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Дон-Аминадо',
+        url: 'https://my.mail.ru/music/search/%D0%94%D0%BE%D0%BD-%D0%90%D0%BC%D0%B8%D0%BD%D0%B0%D0%B4%D0%BE%20(%D1%87%D0%B8%D1%82.%20%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B9%20%D0%9A%D1%83%D1%80%D0%BD%D0%BE%D1%81%D0%BE%D0%B2)?ysclid=lz0905xr3i779378318'
+    },
+    {
+        type: StoryType.HEADER1,
+        caption: 'Саша Чёрный. Юмористические рассказы'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Саша Чёрный. «Красный камешек»',
+        url: 'https://knigavuhe.org/book/krasnyjj-kameshek/'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Саша Чёрный. «Самое страшное»',
+        url: 'https://knigavuhe.org/book/samoe-strashnoe/'
+    },
+    {
+        type: StoryType.HEADER1,
+        caption: 'Зарубежная литература'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Д. Дефо. Роман «Робинзон Крузо»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/robinzon-kruzo?ysclid=lz09chtpfj786524512'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Г.Х. Андерсон. Сказка «Снежная королева»',
+        url: 'https://audioskazki-online.ru/zarubezhnie-audioskazki/andersen/snezhnaya-koroleva?ysclid=lz09dzjhub127347932'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'М. Твен. «Приключения Тома Сойера»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/priklyucheniya-toma-sojera?ysclid=lz09eub9to769982346'
+    },
+    {
+        type: StoryType.STORY,
+        caption: 'Дж. Лондон. «Сказание о Кише»',
+        url: 'https://audioskazki-online.ru/rasskazy/sbornik/skazanie-o-kishe?ysclid=lz09f9pxf4831694872'
     }
 ];
